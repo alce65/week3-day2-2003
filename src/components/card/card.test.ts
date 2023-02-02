950fc5b201a4934c173591fa143d4f2ecd368864
Alejandro Cerezo <alce65@hotmail.es>
2023-02-02 17:15:56 +0100

    ✅ Add test for card component

--- src/components/card/card.test.ts
+++ src/components/card/card.test.ts
@@ -1,19 +1,35 @@
-import { screen } from '@testing-library/dom';
+import { screen, fireEvent } from '@testing-library/dom';
 import '@testing-library/jest-dom';
 import { Card } from './card';
 import { TaskStructure } from '../../models/task';
-import { TASK } from '../../mocks/tasks';
+import { TASKS } from '../../mocks/tasks';
 
 describe('Given Card component', () => {
-  test('It should be in the document', () => {
+  const deleteMock = jest.fn();
+  const updateMock = jest.fn();
+  const mockTask: TaskStructure = TASKS[0];
+  let element: Card;
+  beforeEach(() => {
     document.body.innerHTML = '<ul></ul>';
-    const mockTask: TaskStructure = TASK[0];
-    const element = new Card('ul', mockTask);
+    element = new Card('ul', mockTask, deleteMock, updateMock);
+  });
+  test('It should be in the document', () => {
     expect(element).toBeInstanceOf(Card);
-
+  });
+  test('It render the card in the document', () => {
     const li = screen.getByRole('listitem');
     expect(li).toBeInTheDocument();
     const span = screen.getByText(mockTask.name);
     expect(span).toBeInTheDocument();
+  });
+  test('It should be used the checkbox', () => {
+    const check = screen.getByRole('checkbox');
+    fireEvent.change(check);
+    expect(updateMock).toHaveBeenCalled();
+  });
+  test('It should be used the button', () => {
+    const button = screen.getByRole('button');
+    fireEvent.click(button);
+    expect(deleteMock).toHaveBeenCalled();
   });
 });
